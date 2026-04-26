605b19e9987008486cd11cb30cf83b5352331c79
Ameen E <ayishaameen491@gmail.com>
2026-04-26 20:49:29 +0530

    Add service worker for caching assets

---
+++ service-worker.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'sudoku-pwa-v1';
+const ASSETS_TO_CACHE = [
+    './',
+    './index.html',
+    './style.css',
+    './script.js',
+    './manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+        .then((cache) => cache.addAll(ASSETS_TO_CACHE))
+        .then(() => self.skipWaiting())
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+    return self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+        .then((response) => {
+            // Return cached version or fetch from network
+            return response || fetch(event.request);
+        })
+    );
+});
